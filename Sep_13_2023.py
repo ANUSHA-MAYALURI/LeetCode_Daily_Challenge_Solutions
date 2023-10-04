e6a6b4c9e8ab34e39cec11c4238b15776b36ec98
ANUSHA-MAYALURI <102647671+ANUSHA-MAYALURI@users.noreply.github.com>
2023-10-04 20:30:09 +0530

    Add files via upload

---
+++ Sep_13_2023.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n  # Initialize candies for all children to 1
+        
+        # Forward pass to ensure higher-rated children get more candies
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        # Backward pass to fix any inconsistencies
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        
+        # Calculate the total number of candies
+        total_candies = sum(candies)
+        
+        return total_candies
